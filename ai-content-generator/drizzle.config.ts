f566722a4926cd88472b431a7e239ccd904a9597
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-19 15:41:50 -0600

    drizzle Studio

--- ai-content-generator/drizzle.config.ts
+++ ai-content-generator/drizzle.config.ts
@@ -12,6 +12,7 @@ export default {
   dialect: "postgresql",
   schema: "./utils/schema.tsx",
   dbCredentials:{
-    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL
+    // url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL
+    url : "postgresql://AI_Content-Generator_owner:npg_pOAn5m7VvuLZ@ep-round-breeze-a5a012au-pooler.us-east-2.aws.neon.tech/AI_Content-Generator?sslmode=require"
   }
 };
