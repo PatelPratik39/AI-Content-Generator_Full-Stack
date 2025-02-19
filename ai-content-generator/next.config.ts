aaee7ffdcf991cf0210f4ad5607c10cfadd87762
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-19 14:54:21 -0600

    drizzle ORM and Neon Database for postgresql

--- ai-content-generator/next.config.ts
+++ ai-content-generator/next.config.ts
@@ -1,21 +1,4 @@
-// import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   images: {
-//     domains: [ 'cdn-icons-png.flaticon.com' ]
-//   }
-//   // images: {
-//   //   remotePatterns: [
-//   //     {
-//   //       protocol: "https",
-//   //       hostname: "cdn-icons-png.flaticon.com",
-//   //     }
-//   //   ],
-//   // }
-// };
-
-// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
---
+++ ai-content-generator/utils/db.tsx
@@ -0,0 +1,6 @@
+// @ts-ignore
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL!);
+const db = drizzle(sql);
